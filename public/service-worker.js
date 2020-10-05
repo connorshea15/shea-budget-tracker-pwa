0aa574e516fd8321ab15e7b51bae3e7f2c13b023
connorshea15 <connorshea15@gmail.com>
2020-10-04 17:58:51 -0700

    I cannot figure out this service-worker shinanegans

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,18 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_07';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    "../index.html",
-    "../css/styles.css",
-    "./index.js",
-    "./idb.js"
+    "./index.html",
+    "./css/styles.css",
+    "./js/index.js",
+    "./js/idb.js",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
   ];
 
   self.addEventListener('install', function (e) {
@@ -37,7 +45,7 @@ const FILES_TO_CACHE = [
     );
   });
 
-  self.addEventListener('fetch', function (e) {
+ self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
       caches.match(e.request).then(function (request) {
